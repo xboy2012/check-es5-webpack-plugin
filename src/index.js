f3f67b22ce0b70d49fac8bd59072b750d309c8d3
juemingzi <juemingzi@wacai.com>
2018-11-18 01:01:58 +0800

    update

--- src/index.js
+++ src/index.js
@@ -3,6 +3,21 @@ const colors = require('colors/safe');
 
 const PLUGIN_NAME = 'CheckES5WebpackPlugin';
 
+const tapCompilerEmitHook = (compiler, fn) => {
+  if (compiler.hooks) {
+    compiler.hooks.emit.tapPromise(PLUGIN_NAME, async compilation => fn(compilation));
+  } else {
+    compiler.plugin('emit', async (compilation, callback) => {
+      try {
+        await fn(compilation)
+        callback(null)
+      } catch (e) {
+        callback(e)
+      }
+    });
+  }
+}
+
 const log = (msg) => {
   console.log(`[${PLUGIN_NAME}]${msg}`);
 }
@@ -21,7 +36,7 @@ const check = (source) => {
 
 module.exports = class CheckES5WebpackPlugin {
   apply(compiler) {
-    compiler.hooks.emit.tapPromise(PLUGIN_NAME, async compilation => {
+    tapCompilerEmitHook(compiler, async compilation => {
       const assets = compilation.assets;
       const assetsFiles = Object.keys(assets).filter(fileName => fileName.endsWith('.js'));
       const errorJsFiles = assetsFiles.filter(fileName => !check(assets[fileName]));
@@ -35,6 +50,6 @@ module.exports = class CheckES5WebpackPlugin {
         log(colors.red(` \`${fileName}\` is not ES5 compatible.`));
       }
       throw Error('Some js files are not ES5 compatible.');
-    });
+    })
   }
 };
