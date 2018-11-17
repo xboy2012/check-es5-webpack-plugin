1e8416f0fe4742420a6194df9ec581f3c7f25f97
juemingzi <juemingzi@wacai.com>
2018-11-17 20:33:29 +0800

    beautify console log info

--- src/index.js
+++ src/index.js
@@ -1,29 +1,40 @@
-const acorn = require('acorn')
-const colors = require('colors/safe')
+const acorn = require('acorn');
+const colors = require('colors/safe');
 
-const check = (fileName, source) => {
-  const code = source.source()
+const PLUGIN_NAME = 'CheckES5WebpackPlugin';
+
+const log = (msg) => {
+  console.log(`[${PLUGIN_NAME}]${msg}`);
+}
+
+const check = (source) => {
+  const code = source.source();
   try {
     acorn.parse(code, {
       ecmaVersion: 5
     });
     return true;
   } catch (e) {
-    console.log(colors.red(`File ${fileName} is not ES5 compatible`));
-    throw Error(`File ${fileName} is not ES5 compatible`);
+    return false;
   }
 }
 
-
 module.exports = class CheckES5WebpackPlugin {
   apply(compiler) {
-    compiler.hooks.emit.tapPromise("CheckES5WebpackPlugin", async compilation => {
+    compiler.hooks.emit.tapPromise(PLUGIN_NAME, async compilation => {
       const assets = compilation.assets;
       const assetsFiles = Object.keys(assets).filter(fileName => fileName.endsWith('.js'));
+      const errorJsFiles = assetsFiles.filter(fileName => check(assets[fileName]));
 
-      for(let assetFile of assetsFiles) {
-        check(assetFile, assets[assetFile])
+      if (!errorJsFiles.length) {
+        log(colors.green(`All js files are ES5 compatible.`));
+        return;
       }
+
+      for (let fileName of errorJsFiles) {
+        log(colors.red(` \`${fileName}\` is not ES5 compatible.`));
+      }
+      throw Error('Some js files are not ES5 compatible.');
     });
   }
 };
