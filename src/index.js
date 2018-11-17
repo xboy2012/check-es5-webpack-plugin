8fded8dcf3e5e4e5a5cdbbd8a318991bb6a9e959
juemingzi <juemingzi@wacai.com>
2018-11-17 18:49:30 +0800

    check in

---
+++ src/index.js
@@ -0,0 +1,27 @@
+const acorn = require('acorn')
+
+const check = (fileName, source) => {
+  const code = source.source()
+  try {
+    acorn.parse(code, {
+      ecmaVersion: 5
+    });
+    return true;
+  } catch (e) {
+    throw Error(`File ${fileName} is not ES5 compatible`);
+  }
+}
+
+
+module.exports = class CheckES5WebpackPlugin {
+  apply(compiler) {
+    compiler.hooks.emit.tapPromise("CheckES5WebpackPlugin", async compilation => {
+      const assets = compilation.assets;
+      const assetsFiles = Object.keys(assets).filter(fileName => fileName.endsWith('.js'));
+
+      for(let assetFile of assetsFiles) {
+        check(assetFile, assets[assetFile])
+      }
+    });
+  }
+};
