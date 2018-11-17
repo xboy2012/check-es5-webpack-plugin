49d3e6f924b3b858466fc24da661b1e958ee5478
juemingzi <juemingzi@wacai.com>
2018-11-17 19:40:32 +0800

    update

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const check = (fileName, source) => {
     });
     return true;
   } catch (e) {
-    console.error(colors.error(`File ${fileName} is not ES5 compatible`));
+    console.log(colors.error(`File ${fileName} is not ES5 compatible`));
     throw Error(`File ${fileName} is not ES5 compatible`);
   }
 }
